fb535294805ebeaa0769d4ba0805c6d441c73301
renatasantox <renatarafaelapereiradejesis@gmail.com>
2025-08-13 08:33:46 -0300

    Cena jogo

---
+++ Etapa 2 - POO UI/Assets/Scripts/Jogador.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Jogador : Personagem
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space.A)) //Esquerda
+        {
+            transform.position
+        }
+    }
+}
